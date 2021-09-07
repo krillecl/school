247d3cb14943f938e56e38030b5b225bc4f06cad
Kristoffer Lund <kristoffercl@hotmail.com>
2021-09-07 07:17:42 +0200

    main

---
+++ index.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Madframor</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.css"/>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
+
+</head>
+<body>
+
+    <header class="header">
+
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+
+            <div class="navbar-brand">
+
+                <a class="navbar-item" href="https://bulma.io">
+                    <img src="/images/logo/logo-dark.svg">
+                </a>
+            
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+
+            </div>
+          
+            <div id="navbar" class="navbar-menu">
+
+                <div class="navbar-start">
+
+                    <a class="navbar-item">
+                        Opskrifter
+                    </a>
+          
+                    <a class="navbar-item">
+                        Madplaner
+                    </a>
+
+                    <a class="navbar-item">
+                        Om bloggen
+                    </a>
+
+                    <a class="navbar-item">
+                        Kontakt
+                    </a>
+            
+                </div>
+          
+            </div>
+
+            <div class="search-bar">
+
+                <span class="material-icons">
+                    search
+                </span>
+                    
+            </div>
+
+        </nav>
+
+    </header>
+
+    <main class="main">
+
+        <section class="hero">
+
+            <div class="hero-body">
+
+            <div class="card">
+
+                <div class="card-image">
+
+                    <figure class="image is-4by3">
+
+                        <img src="images/smoothie.jpeg" alt="Placeholder image">
+                        
+                    </figure>
+
+                </div>
+
+                <div class="card-content">
+
+                    <div class="content">
+
+                        <div class="card-category">
+                            <a href="#">Bagværk</a>
+                        </div>
+
+                        <p class="card-title">
+                            <a href="#">Hjemmebagte Surdejsboller</a>
+                        </p>
+                        
+                        <p>Jeg har gennem det sidste halv &aring;r brugt lang tid p&aring; at justere min surdej og opskrifte p&aring; mine boller. Det er endt ud i disse dejlige, bl&oslash;de og luftige surdejsboller</p>
+
+                        <div class="card-time">
+                            <time datetime="2016-1-1">06. Sep, 2021</time>
+                        </div>
+
+                    </div> <!-- /.content -->
+
+                </div> <!-- /.card-content -->
+
+                </div> <!-- /.card -->
+
+            </div> <!-- /.hero-body  -->
+
+        </section>
+
+        <div class="list list-blue is-max-widescreen" data-simplebar>
+
+            <a class="list-item">Aftensmad</a>
+            <a class="list-item">Bagværk</a>
+            <a class="list-item">Dessert</a>
+            <a class="list-item">Frokost</a>
+            <a class="list-item">Morgenmad</a>
+            <a class="list-item">Tilbehør</a>
+
+        </div> <!-- /.list -->
+
+        <div class="container is-max-widescreen">
+
+            <div class="columns is-flex-wrap-wrap">
+
+            <div class="column is-one-third">
+
+                <div class="card">
+
+                    <div class="card-image">
+
+                        <figure class="image is-4by3">
+
+                            <img src="images/smoothie.jpeg" alt="Placeholder image">
+                            
+                        </figure>
+
+                    </div>
+
+                    <div class="card-content">
+
+                        <div class="content">
+
+                            <div class="card-category">
+                                <a href="#">Bagværk</a>
+                            </div>
+
+                            <p class="card-title">
+                                <a href="#">Hjemmebagte Surdejsboller</a>
+                            </p>
+                            
+                            <p>Jeg har gennem det sidste halv &aring;r brugt lang tid p&aring; at justere min surdej og opskrifte p&aring; mine boller. Det er endt ud i disse dejlige, bl&oslash;de og luftige surdejsboller</p>
+
+                            <div class="card-time">
+                                <time datetime="2016-1-1">06. Sep, 2021</time>
+                            </div>
+
+                        </div> <!-- /.content -->
+
+                    </div> <!-- /.card-content -->
+
+                </div> <!-- /.card -->
+
+                </div> <!-- /.column --> 
+
+                <div class="column is-one-third">
+
+                <div class="card">
+
+                    <div class="card-image">
+
+                        <figure class="image is-4by3">
+
+                            <img src="images/smoothie.jpeg" alt="Placeholder image">
+                            
+                        </figure>
+
+                    </div>
+
+                    <div class="card-content">
+
+                        <div class="content">
+
+                            <div class="card-category">
+                                <a href="#">Bagværk</a>
+                            </div>
+
+                            <p class="card-title">
+                                <a href="#">Hjemmebagte Surdejsboller</a>
+                            </p>
+                            
+                            <p>Jeg har gennem det sidste halv &aring;r brugt lang tid p&aring; at justere min surdej og opskrifte p&aring; mine boller. Det er endt ud i disse dejlige, bl&oslash;de og luftige surdejsboller</p>
+
+                            <div class="card-time">
+                                <time datetime="2016-1-1">06. Sep, 2021</time>
+                            </div>
+
+                        </div> <!-- /.content -->
+
+                    </div> <!-- /.card-content -->
+
+                </div> <!-- /.card -->
+
+                </div> <!-- /.column --> 
+
+                <div class="column is-one-third">
+
+                <div class="card">
+
+                    <div class="card-image">
+
+                        <figure class="image is-4by3">
+
+                            <img src="images/smoothie.jpeg" alt="Placeholder image">
+                            
+                        </figure>
+
+                    </div>
+
+                    <div class="card-content">
+
+                        <div class="content">
+
+                            <div class="card-category">
+                                <a href="#">Bagværk</a>
+                            </div>
+
+                            <p class="card-title">
+                                <a href="#">Hjemmebagte Surdejsboller</a>
+                            </p>
+                            
+                            <p>Jeg har gennem det sidste halv &aring;r brugt lang tid p&aring; at justere min surdej og opskrifte p&aring; mine boller. Det er endt ud i disse dejlige, bl&oslash;de og luftige surdejsboller</p>
+
+                            <div class="card-time">
+                                <time datetime="2016-1-1">06. Sep, 2021</time>
+                            </div>
+
+                        </div> <!-- /.content -->
+
+                    </div> <!-- /.card-content -->
+
+                </div> <!-- /.card -->
+
+                </div> <!-- /.column --> 
+
+                <div class="column is-one-third">
+
+                <div class="card">
+
+                    <div class="card-image">
+
+                        <figure class="image is-4by3">
+
+                            <img src="images/smoothie.jpeg" alt="Placeholder image">
+                            
+                        </figure>
+
+                    </div>
+
+                    <div class="card-content">
+
+                        <div class="content">
+
+                            <div class="card-category">
+                                <a href="#">Bagværk</a>
+                            </div>
+
+                            <p class="card-title">
+                                <a href="#">Hjemmebagte Surdejsboller</a>
+                            </p>
+                            
+                            <p>Jeg har gennem det sidste halv &aring;r brugt lang tid p&aring; at justere min surdej og opskrifte p&aring; mine boller. Det er endt ud i disse dejlige, bl&oslash;de og luftige surdejsboller</p>
+
+                            <div class="card-time">
+                                <time datetime="2016-1-1">06. Sep, 2021</time>
+                            </div>
+
+                        </div> <!-- /.content -->
+
+                    </div> <!-- /.card-content -->
+
+                </div> <!-- /.card -->
+
+                </div> <!-- /.column --> 
+
+                <div class="column is-one-third">
+
+                    <div class="card">
+
+                        <div class="card-image">
+
+                            <figure class="image is-4by3">
+
+                                <img src="images/smoothie.jpeg" alt="Placeholder image">
+
+                            </figure>
+
+                        </div>
+
+                        <div class="card-content">
+
+                            <div class="content">
+
+                                <div class="card-category">
+                                    <a href="#">Bagværk</a>
+                                </div>
+
+                                <p class="card-title">
+                                    <a href="#">Hjemmebagte Surdejsboller</a>
+                                </p>
+                                
+                                <p>Jeg har gennem det sidste halv &aring;r brugt lang tid p&aring; at justere min surdej og opskrifte p&aring; mine boller. Det er endt ud i disse dejlige, bl&oslash;de og luftige surdejsboller</p>
+
+                                <div class="card-time">
+                                    <time datetime="2016-1-1">06. Sep, 2021</time>
+                                </div>
+
+                            </div> <!-- /.content -->
+
+                        </div> <!-- /.card-content -->
+
+                    </div> <!-- /.card -->
+
+                </div> <!-- /.column --> 
+
+            </div> <!-- /.columns -->
+             
+        </div> <!-- /.container -->
+
+    </main>
+
+    <footer class="footer">
+
+        <div class="footer__container">
+
+            <div class="copyright has-text-centered">
+
+                &copy; Copyright www.madframor.dk 2021. All rights reserved
+
+            </div>
+
+        </div>
+
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js"></script>
+    <script src="js/app.js"></script>
+
+</body>
+</html>
