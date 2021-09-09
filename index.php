1208b3c7059b2a252e29d43ce5fd553beada0e33
Kristoffer Lund <kristoffercl@hotmail.com>
2021-09-09 07:23:07 +0200

    v4

--- index.php
+++ index.php
@@ -23,16 +23,22 @@
 
         <nav class="navbar" role="navigation" aria-label="main navigation">
 
-            <div class="navbar-brand">
+            <div class="burger">
 
-                <a class="navbar-item" href="https://bulma.io">
-                    <img src="/images/logo/logo-dark.svg">
-                </a>
-            
                 <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
+                </a>
+
+            </div>
+
+            <div class="navbar-brand">
+
+                <a class="navbar-item" href="https://bulma.io">
+
+                    <img src="/images/logo/logo-dark.svg">
+                    
                 </a>
 
             </div>
