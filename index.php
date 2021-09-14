bb964fedebd861405393ec9901781b55de9d03f4
Kristoffer Lund <kristoffercl@hotmail.com>
2021-09-14 07:15:14 +0200

    v1

--- index.php
+++ index.php
@@ -4,400 +4,202 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Madframor</title>
 
+    <!-- Google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.css"/>
-
+    <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/utilities.css">
     <link rel="stylesheet" href="css/stylesheet.css">
 
+    <title>blog</title>
 </head>
-<body>
+<body class="body">
 
     <header class="header">
 
-        <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="topbar width">
 
-            <div class="burger">
+            <div class="brand">
 
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
+                <div class="brand__logo">
 
-            </div>
+                    <a href="#"> <img src="/images/logo/logo-dark.svg"> </a>
 
-            <div class="navbar-brand">
-
-                <a class="navbar-item" href="https://bulma.io">
-
-                    <img src="/images/logo/logo-dark.svg">
-                    
-                </a>
-
-            </div>
-          
-            <div id="navbar" class="navbar-menu">
-
-                <div class="navbar-start">
-
-                    <a class="navbar-item">
-                        Opskrifter
-                    </a>
-          
-                    <a class="navbar-item">
-                        Madplaner
-                    </a>
-
-                    <a class="navbar-item">
-                        Om bloggen
-                    </a>
-
-                    <a class="navbar-item">
-                        Kontakt
-                    </a>
-            
                 </div>
-          
-            </div>
+
+            </div> <!-- /.brand -->
+
+            <nav class="main_menu">
+                <a class="main_menu__link" href="#">Opskrifter</a>
+                <a class="main_menu__link" href="#">Madplaner</a>
+                <a class="main_menu__link" href="#">Kontakt</a>
+            </nav>
 
             <div class="search-form">
-                <form action="{{ route('search') }}" method="GET">
-
-                    <input placeholder="Søg efter ingredienser, opskrifter m.m." type="text" name="search" required/>
-                    <button type="submit"><i class="fas fa-search"></i></button>
-
-                </form>
-            </div>
-
-            <div class="search-bar">
 
                 <span class="material-icons">
                     search
                 </span>
-                    
+
             </div>
-
-        </nav>
-
-        <aside class="category-list" data-simplebar>
-
-            <a class="category-list-item">Aftensmad</a>
-            <a class="category-list-item">Bagværk</a>
-            <a class="category-list-item">Frokost</a>
-            <a class="category-list-item">Morgenmad</a>
-            <a class="category-list-item">Tilbehør</a>  
-
-        </aside>
-
-        <section class="hero">
-
-            <div class="hero-body">
-
-                <div class="card featured">
-
-                    <div class="card-image">
-
-                        <figure class="image">
-
-                            <a href="#"><img src="images/broed.jpeg" alt="Placeholder image"></a>
-                            
-                        </figure>
-
-                    </div>
-
-                    <div class="card-content">
-
-                        <div class="content">
-
-                            <div class="card-category">
-                                <a class="card-blue" href="#">Bagværk</a>
-                            </div>    
-
-                            <p class="card-title card-headline">
-                                <a href="#">De Nemme Surdejsboller</a>
-                            </p>
-
-                        </div> <!-- /.content -->
-
-                    </div> <!-- /.card-content -->
-
-                </div> <!-- /.card -->
-
-                <aside class="recommended">
-
-                    <div class="list">
-                            
-                        <div class="card-headline">
-                            Vores udvalgte
-                        </div>
-
-                        <div class="list-item">
-
-                            <div class="card-category">
-                                <a class="card-blue" href="#">Bagværk</a>
-                            </div>    
-
-                            <p class="card-title">
-                                <a href="#">De Nemme Surdejsboller</a>
-                            </p>
-
-                        </div>
-
-                        <div class="list-item">
-
-                            <div class="card-category">
-                                <a class="card-blue" href="#">Bagværk</a>
-                            </div>    
-
-                            <p class="card-title">
-                                <a href="#">De Nemme Surdejsboller</a>
-                            </p>
-
-                        </div>
-
-                        <div class="list-item">
-
-                            <div class="card-category">
-                                <a class="card-blue" href="#">Bagværk</a>
-                            </div>    
-
-                            <p class="card-title">
-                                <a href="#">De Nemme Surdejsboller</a>
-                            </p>
-
-                        </div>
-
-                        <div class="list-item">
-
-                            <div class="card-category">
-                                <a class="card-blue" href="#">Bagværk</a>
-                            </div>    
-
-                            <p class="card-title">
-                                <a href="#">De Nemme Surdejsboller</a>
-                            </p>
-
-                        </div>
-
-                    </div>
-
-                </aside>
-
-            </div> <!-- /.hero-body  -->
-
-        </section>
-
-    </header>
-
-    <main class="main">
-
-        <section class="articles">
-
-            <div class="container is-max-widescreen">
-
-                <div class="columns is-flex-wrap-wrap">
-
-                <div class="column is-one-third">
-
-                    <div class="card">
-
-                        <div class="card-image">
-
-                            <figure class="image is-4by3">
-
-                                <a href="#"><img src="images/broed.jpeg" alt="Placeholder image"></a>
-                                
-                            </figure>
-
-                        </div>
-
-                        <div class="card-content">
-
-                            <div class="content">
-
-                                <div class="card-category">
-                                    <a class="card-blue" href="#">Bagværk</a>
-                                </div>    
-
-                                <p class="card-title">
-                                    <a href="#">De Nemme Surdejsboller</a>
-                                </p>
-
-                            </div> <!-- /.content -->
-
-                        </div> <!-- /.card-content -->
-
-                    </div> <!-- /.card -->
-
-                    </div> <!-- /.column --> 
-
-                    <div class="column is-one-third">
-
-                    <div class="card">
-
-                        <div class="card-image">
-
-                            <figure class="image is-4by3">
-
-                                <a href="#"><img src="images/pasta.jpeg" alt="Placeholder image"></a>
-                                
-                            </figure>
-
-                        </div>
-
-                        <div class="card-content">
-
-                            <div class="content">
-
-                                <div class="card-category">
-                                    <a class="card-red" href="#">Aftensmad</a>
-                                </div>    
-
-                                <p class="card-title">
-                                    <a href="#">Hjemmebagte Surdejsboller</a>
-                                </p>
-
-                            </div> <!-- /.content -->
-
-                        </div> <!-- /.card-content -->
-
-                    </div> <!-- /.card -->
-
-                    </div> <!-- /.column --> 
-
-                    <div class="column is-one-third">
-
-                    <div class="card">
-
-                        <div class="card-image">
-
-                            <figure class="image is-4by3">
-
-                                <a href="#"><img src="images/Ragu.jpeg" alt="Placeholder image"></a>
-                                
-                            </figure>
-
-                        </div>
-
-                        <div class="card-content">
-
-                            <div class="content">
-
-                                <div class="card-category">
-                                    <a class="card-red" href="#">Aftensmad</a>
-                                </div>    
-
-                                <p class="card-title">
-                                    <a href="#">Ragu</a>
-                                </p>
-
-                            </div> <!-- /.content -->
-
-                        </div> <!-- /.card-content -->
-
-                    </div> <!-- /.card -->
-
-                    </div> <!-- /.column --> 
-
-                    <div class="column is-one-third">
-
-                    <div class="card">
-
-                        <div class="card-image">
-
-                            <figure class="image is-4by3">
-
-                                <a href="#"><img src="images/pasta.jpeg" alt="Placeholder image"></a>
-                                
-                            </figure>
-
-                        </div>
-
-                        <div class="card-content">
-
-                            <div class="content">
-
-                                <div class="card-category">
-                                    <a class="card-blue" href="#">Bagværk</a>
-                                </div>    
-
-                                <p class="card-title">
-                                    <a href="#">Hjemmebagte Surdejsboller</a>
-                                </p>
-                                
-                            </div> <!-- /.content -->
-
-                        </div> <!-- /.card-content -->
-
-                    </div> <!-- /.card -->
-
-                    </div> <!-- /.column --> 
-
-                    <div class="column is-one-third">
-
-                        <div class="card">
-
-                            <div class="card-image">
-
-                                <figure class="image is-4by3">
-
-                                    <a href="#"><img src="images/smoothie.jpeg" alt="Placeholder image"></a>
-
-                                </figure>
-
-                            </div>
-
-                            <div class="card-content">
-
-                                <div class="content"> 
-                                    
-                                    <div class="card-category">
-                                        <a class="card-green" href="#">Tilbehør</a>
-                                    </div>        
-
-                                    <p class="card-title">
-                                        <a href="#">Hjemmebagte Surdejsboller</a>
-                                    </p>
-                                    
-                                </div> <!-- /.content -->
-
-                            </div> <!-- /.card-content -->
-
-                        </div> <!-- /.card -->
-
-                    </div> <!-- /.column --> 
-
-                </div> <!-- /.columns -->
-                
-            </div> <!-- /.container -->
         
-        </section>
+        </div> <!-- /.topbar -->
 
-    </main>
-
-    <footer class="footer">
-
-        <div class="footer__container">
-
-            <div class="copyright has-text-centered">
-
-                &copy; Copyright www.madframor.dk 2021. All rights reserved
-
+        <div class="sub-bar">
+            <div class="category_menu">
+                <a class="category_menu__link" href="#">Aftensmad</a>
+                <a class="category_menu__link" href="#">Bagværk</a>
+                <a class="category_menu__link" href="#">Frokost</a>
+                <a class="category_menu__link" href="#">Morgenmad</a>
+                <a class="category_menu__link" href="#">TIlbehør</a>
             </div>
 
         </div>
 
+    </header>
+
+    <main class="main width">
+
+        <section class="articles section">
+
+            <div class="section__headline">De seneste opskrifter</div>
+
+            <div class="grid">
+
+                <article class="article">
+
+                    <div class="card">
+
+                        <div class="card-image">
+
+                            <figure class="image">
+
+                                <a href="#"><img src="images/smoothie.jpeg" alt="Placeholder image"></a>
+                                
+                            </figure>
+
+                        </div>
+
+                        <div class="card-content">
+
+                            <div class="content">
+
+                                <p class="card__category">
+                                    <a href="#">Morgenmad</a>
+                                </p>
+
+                                <p class="card__title">
+                                    <a href="#">Godmorgen Smoothie</a>
+                                </p>
+
+                            </div> <!-- /.content -->
+
+                        </div> <!-- /.card-content -->
+
+                    </div> <!-- /.card -->
+
+                </article>
+
+                <article class="article">
+
+                    <div class="card">
+
+                        <div class="card-image">
+
+                            <figure class="image">
+
+                                <a href="#"><img src="images/smoothie.jpeg" alt="Placeholder image"></a>
+                                
+                            </figure>
+
+                        </div>
+
+                        <div class="card-content">
+
+                            <div class="content">
+
+                                <p class="card__category">
+                                    <a href="#">Morgenmad</a>
+                                </p>
+
+                                <p class="card__title">
+                                    <a href="#">Godmorgen Smoothie</a>
+                                </p>
+
+                            </div> <!-- /.content -->
+
+                        </div> <!-- /.card-content -->
+
+                    </div> <!-- /.card -->
+
+                </article>
+
+                <article class="article">
+
+                    <div class="card">
+
+                        <div class="card-image">
+
+                            <figure class="image">
+
+                                <a href="#"><img src="images/smoothie.jpeg" alt="Placeholder image"></a>
+                                
+                            </figure>
+
+                        </div>
+
+                        <div class="card-content">
+
+                            <div class="content">
+
+                                <p class="card__category">
+                                    <a href="#">Morgenmad</a>
+                                </p>
+
+                                <p class="card__title">
+                                    <a href="#">Godmorgen Smoothie</a>
+                                </p>
+
+                            </div> <!-- /.content -->
+
+                        </div> <!-- /.card-content -->
+
+                    </div> <!-- /.card -->
+
+                </article>
+
+            </div>
+
+        </section>
+
+    </main>
+
+    <footer class="footer width">
+
+        <div class="footer__copyright">
+
+            Copyright &copy; <a href="#">www.madframor.dk</a> <?php echo date('Y'); ?>
+
+        </div> <!-- /.footer__copyright -->
+
+        <nav class="footer__nav nav">
+
+            <div class="nav__menu">
+                <a class="nav__link" href="#">Opskrifter</a>
+                <a class="nav__link" href="#">Madplaner</a>
+                <a class="nav__link" href="#">Kontakt</a>
+            </div>
+
+        </nav>
+
     </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js"></script>
-    <script src="js/app.js"></script>
-
+    
 </body>
 </html>
